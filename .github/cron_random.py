d24f8d2d6b302cb4b18cf8c910263a16f717b075
Juijote <22154581+Juijote@users.noreply.github.com>
2023-06-30 12:30:07 +0800

    Update cron_random.py

--- .github/cron_random.py
+++ .github/cron_random.py
@@ -1,14 +1,13 @@
 # coding:utf-8 
 import random
 min=random.randint(0, 59)
-h=random.randint(0, 23)
 
 
 YML=".github/workflows/circle_translate.yml"
 
 f = open(YML, "r+", encoding="UTF-8")
 list1 = f.readlines()           
-list1[7] = "   - cron: '%d %d * * *'\n"%(min,h)
+list1[7] = "   - cron: '%d */6 * * *'\n"%(min)
 
 f = open(YML, "w+", encoding="UTF-8")
 f.writelines(list1)
